da252c05adfca588e1398fd33096c94c04367058
Craig <Craigm@98115.net>
2011-04-08 15:05:05 -0700

    clean up server source and move to go-gb for building

--- Server/buildrun.sh
+++ Server/buildrun.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
 gotgo -o=tcp/connectedBag.go -package-name=tcp iterbag.got LoggedIn
-
-
-# 6g -o tcp.6 tcp/*.go
-# 6g main.go
-# 6l main.6
-# ./6.out
-
-
-DEPS="tcp"
-for dep in ${DEPS}; do
-	cd $dep ; gomake ; cd ..
-done
-# gomake clean
-gomake
-
-6l -L tcp/_obj _go_.6
-./6.out
+gb
+./main
